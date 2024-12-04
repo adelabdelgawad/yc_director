b45ac1e2c3fc5af80d2d77131b1bd6111cf24d76
unknown <tech.adel87@gmail.com>
2024-12-04 07:42:43 +0200

    Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-white" >Home</h1>
+      <section className="pink_container">
+        <h1 className="heading" >Pitch Your Startup <br /> Connect With Enterpreneurs</h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and get Noticed in Virtual Competitions
+        </p>
+      </section>
     </>
   );
 }
